04229dfd58d6c74239d59509c150fd303569707b
bgarcia95 <bgarcia.95@outlook.com>
2022-12-20 15:58:08 -0600

    call methods

--- bin/packages.dart
+++ bin/packages.dart
@@ -1,5 +1,6 @@
 import 'package:packages/packages.dart' as packages;
 
 void main(List<String> arguments) {
-  print('Hello world: ${packages.calculate()}!');
+  // packages.getReqResService();
+  packages.getCountryInfo();
 }
